97e8c60077ea4564766e68125acd5fe27ac34451
apresent-PC\apresent <amailforxx@gmail.com>
2017-09-24 17:16:28 +0900

    no message

---
+++ Assets/script/FallinChecker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallinChecker : MonoBehaviour {
+	public hole red,blue,green;
+
+	void OnGUI(){
+		string label = " ";
+		if(red.isFallIn() && blue.isFallIn() && green.isFallIn()){
+			label = "PON";
+		}
+
+		GUI.Label(new Rect(0, 0, 100, 30), label);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/script/hole.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hole : MonoBehaviour {
+	bool fallin;
+	public string activeTag;
+
+	public bool isFallIn(){
+		return fallin;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == activeTag){
+			fallin = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.tag == activeTag){
+			fallin = false;
+		}
+	}
+
+	void OnTriggerStay(Collider other){
+		Rigidbody rb = other.gameObject.GetComponent<Rigidbody> ();
+		Vector3 direction = transform.position - other.gameObject.transform.position;
+		direction.Normalize ();
+
+		if(other.gameObject.tag == activeTag){
+			rb.velocity *= 0.9f;
+			rb.AddForce (direction * rb.mass * 20.0f);
+		}else{
+			rb.AddForce (-direction * rb.mass * 80.0f);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
